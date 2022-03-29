bef8c0eb4b8f866f962cd2d691a3475ce3189c69
keishaputritheanny <pkeisha47@gmail.com>
2022-03-29 12:52:44 +0700

    fix

--- app/src/main/java/com/kei/dakomapp/ui/mainFragment/HomeFragment.kt
+++ app/src/main/java/com/kei/dakomapp/ui/mainFragment/HomeFragment.kt
@@ -1,16 +1,19 @@
 package com.kei.dakomapp.ui.mainFragment
-import android.content.Intent
+
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.kei.dakomapp.R
-import com.kei.dakomapp.ui.discoveryFragments.DiscoveryActivity
-import kotlinx.android.synthetic.main.fragment_home.*
+
+class HomeFragment : Fragment() {
 
 
-class HomeFragment : Fragment(), View.OnClickListener{
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,12 +23,6 @@ class HomeFragment : Fragment(), View.OnClickListener{
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        ivDiscoveryHome.setOnClickListener(this)
-
-    }
-
     companion object {
         @JvmStatic
         fun newInstance() =
@@ -33,17 +30,4 @@ class HomeFragment : Fragment(), View.OnClickListener{
                 arguments = Bundle().apply {}
             }
     }
-
-    override fun onClick(p0: View) {
-        when(p0.id) {
-            R.id.ivDiscoveryHome -> moveDiscovery()
-        }
-    }
-
-    private fun moveDiscovery() {
-        val intent = Intent(activity, DiscoveryActivity::class.java)
-        activity?.startActivity(intent)
-    }
-
-
 }
